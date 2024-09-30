8830032f17964d8c26d70098ceb22d5e232780c9
Peter Skrivanek <S00236650@atu.ie>
2024-09-30 12:32:36 +0100

    Add project files.

---
+++ RAD_Lab2/Todo.cs
@@ -0,0 +1,10 @@
+﻿namespace RAD_Lab2
+{
+    public class Todo
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+        public int Priority { get; set; }
+    }
+}
---
+++ RAD_Lab2/TodoDb.cs
@@ -0,0 +1,11 @@
+﻿namespace RAD_Lab2
+{
+    using Microsoft.EntityFrameworkCore;
+
+    public class TodoDb : DbContext
+    {
+        public TodoDb(DbContextOptions<TodoDb> options) : base(options) { }
+
+        public DbSet<Todo> Todos => Set<Todo>();
+    }
+}
